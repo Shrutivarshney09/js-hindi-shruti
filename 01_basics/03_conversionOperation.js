0325ff9bd3d3e97b7e9bc44162ec25cd70fa12b2
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-08-28 15:45:54 +0000

    conversion datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -54,7 +54,15 @@ let str3 = str1 + str2
 
 // console.log((3 + 4) * 5 % 3);
 
-console.log(+true);
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
 
 
 
