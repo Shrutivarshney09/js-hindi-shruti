210dfd5b33464f320c5a124e63699419da205191
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-08-28 16:22:55 +0000

    datatype summary

---
+++ datatypes summary.js
@@ -0,0 +1,37 @@
+// Primitive datatype
+
+// 7 Types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+//const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = ("123")
+
+//console.log(id === anotherId);
+
+//const bigNumber = 22344556677667n
+
+
+
+// Refrence datatype (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "shruti",
+    age: 22,
+}
+
+const myfunction = function () {
+    console.log("hello world");
+    }
+
+    //console.log(typeof bigNumber);
+    //
+    console.log(typeof outsideTemp);
