a021198f4dcd94d1436adb334e67e7ba402b6a23
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-08-28 07:02:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "shrutivarshney09@gmil.com"
+var accountPassword = "12345"
+accountCity = "aligarh"
+let accountState;
+
+//accountId = 2 // not allowed
+accountEmail = "shreya2gmail.com"
+accountPassword = "2233"
+accountCity = "delhi"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
