bd996dab37021070f2c378713f2c1c202868d79a
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-08-29 07:07:45 +0000

    number and math

---
+++ 01_basics/06_number and math.js
@@ -0,0 +1,27 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 233.8859
+
+//console.log(otherNumber.toPrecision(4));
+
+const hunders = 1000000
+//console.log(hunders.toLocaleString('en-IN'));
+
+// ************ MATHS *******************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.9));
+console.log(Math.ceil(5.9));
+console.log(Math.floor(5.9));
+console.log(Math.min(4,6,8,9));
+console.log(Math.max(3, 5, 6, 8, 9));
+
+
