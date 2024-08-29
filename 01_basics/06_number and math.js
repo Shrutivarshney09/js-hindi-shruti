289e78b6d10fea0ce22a654494f9f88eb6dc0fa0
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-08-29 07:22:35 +0000

    number and math

--- 01_basics/06_number and math.js
+++ 01_basics/06_number and math.js
@@ -16,12 +16,19 @@ const hunders = 1000000
 
 // ************ MATHS *******************
 
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(5.9));
-console.log(Math.ceil(5.9));
-console.log(Math.floor(5.9));
-console.log(Math.min(4,6,8,9));
-console.log(Math.max(3, 5, 6, 8, 9));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(5.9));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(4,6,8,9));
+// console.log(Math.max(3, 5, 6, 8, 9));
 
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1))+ min);
