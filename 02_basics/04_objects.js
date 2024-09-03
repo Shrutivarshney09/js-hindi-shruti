b4b58d075c7d9e46ab30337de5a2b60748dc6e83
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-09-03 18:31:14 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,7 +27,7 @@ const obj4 = {5: "a", 6: "b"}
 //const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2, ...obj4}
-console.log(obj3);
+// console.log(obj3);
 
 
 const users = [
@@ -40,10 +40,28 @@ const users = [
 ]
 
 users [1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+const course = {
+   coursename:  "js in hindi",
+   price: "999",
+   teacher: "hitesh chaudhary",
+   location: "online in youtube" 
+}
+
+// course.teacher
+
+const {teacher: instructor} = course 
+console.log(instructor);
+
+
+
+
+
