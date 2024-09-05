efddaddc75afdc9dd7fbf81fc23a8735bbcc779c
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-09-05 19:23:44 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,17 @@
+
+function sayMyName(){
+console.log("s");
+console.log("h");
+console.log("r");
+console.log("u");
+console.log("t");
+console.log("i");
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(5, 8)
