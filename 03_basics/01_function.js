593f241db88ea80c1537c27f336935c6c851fdc3
Shrutivarshney09 <149359428+Shrutivarshney09@users.noreply.github.com>
2024-09-06 16:24:46 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -10,8 +10,31 @@ console.log("i");
 
 // sayMyName()
 
+//function addTwoNumbers(number1, number2){
+  //  console.log(number1 + number2);
+
+//}
+
 function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
+  // let result = number1 + number2
+   //return result
+   
+   return number1 + number2
 }
 
-addTwoNumbers(5, 8)
+const result = addTwoNumbers(5, 8)
+
+//console.log("Result: ", result);
+
+
+function loginUserMessage(username = "shreya"){
+   if(!username){
+      console.log("please enter a username");
+     return
+    }
+    return `${username} just logged in`
+}
+
+//console.log(loginUserMessage("shruti"));
+console.log(loginUserMessage("shruvi"));
+
